a5e4bc0fb212670f36dcfe4955a93c3ca67a8d24
Rodrigo Freitas <rodrigofreitasit@gmail.com>
2018-06-21 18:23:25 -0300

    Projeto Finalizado

---
+++ ContaBancaria/Movimentacao.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria {
+    class Movimentacao {
+
+        public string nome, depositoInicial;
+        public int numeroConta { get; private set; }
+        public double valorInicial;
+
+
+        // CONSTRUTOR OBRIGANDO DIGITAR DADOS//
+
+        public Movimentacao(int numeroConta, string nome, double valorInicial) {
+            this.nome = nome;
+            this.valorInicial = valorInicial;
+            this.numeroConta = numeroConta;
+        }
+
+        public Movimentacao(int numeroConta, string nome) {
+            this.nome = nome;
+            valorInicial = 0.0;
+            this.numeroConta = numeroConta;
+        }
+        //******************DEPOSITO E SAQUE************** //
+
+        public void deposito(double qte) {
+            valorInicial = valorInicial + qte;
+        }
+        public void saque(double qte) {
+            valorInicial = valorInicial - qte - 5.0;
+        }
+
+        public override string ToString() {
+            return
+                "Conta " + numeroConta
+               + ", Titular: " + nome
+               + ", Saldo: R$ " + valorInicial.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
+
+// VALOR INICIAL SIM OU NAO
+/*public bool sim() {
+   if (depositoInicial == "s") {
+       return true;
+   }
+   else {
+       return false;
+   }
+}
+public double nao() {
+   if (sim()) {
+       return valorInicial;
+   }
+   else
+       return valorInicial = 0.0;
+}
+*/
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria {
+    class Program {
+        static void Main(string[] args) {
+
+            double qte;
+            Movimentacao M;
+
+
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do Titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá deposito inicial (s/n)? ");
+            string depositoInicial = Console.ReadLine();
+            if (depositoInicial == "s") {
+                Console.Write("Digite o valor do depósito incial: ");
+                double valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                M = new Movimentacao(numeroConta, nome, valorInicial);
+            }
+            else {
+                M = new Movimentacao(numeroConta, nome);
+            }
+            /*
+                        if (M.sim()) {
+                            Console.Write("Digite o valor do depósito incial: ");
+                            M.valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        }
+                        else {
+                            M.nao();
+                        }
+            */
+            Console.WriteLine();
+            Console.WriteLine("Conta Criada:");
+            Console.WriteLine(M);
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para depósito: ");
+            qte = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            M.deposito(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Conta Atualizada:");
+            Console.WriteLine(M);
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque: ");
+            qte = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            M.saque(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Conta Atualizada:");
+            Console.WriteLine(M);
+
+            Console.ReadLine();
+        }
+    }
+}
